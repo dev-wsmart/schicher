c0394f39e4dc0e8c65cd63a37ffb0b403ad79686
Nongnapat <nongnapat.j@wsmart.co.th>
2020-07-31 10:07:42 +0700

    change link stylesheet

--- application/views/template.php
+++ application/views/template.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/smartsure-style.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>font/stylesheet.css">
+    <link rel="stylesheet" href="<?php base_url(); ?>css/smartsure-style.css">
+    <link rel="stylesheet" href="<?php base_url(); ?>font/stylesheet.css">
 
     <!-- JS, Popper.js, and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
