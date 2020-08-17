e5d483bf62b1550d416d9d994e02906e4b616760
DEV wsmart <nongnapat.j@wsmart.co.th>
2020-08-17 17:34:36 +0700

    Can change language

---
+++ application/controllers/language.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Language extends CI_Controller {
+
+    function index(){
+        $previousUrl = $_GET["url"];
+        $language = $_GET["lang"];
+
+        $this->session->set_userdata("language", $language);
+
+        redirect($previousUrl);
+    }
+
+}
+?>
