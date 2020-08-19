ad23f2a1c023f2d9196d2e9abce69c75104fdb6e
DEV wsmart <nongnapat.j@wsmart.co.th>
2020-08-19 15:11:42 +0700

    Dropdown service

--- application/views/Banner.php
+++ application/views/Banner.php
@@ -138,14 +138,14 @@ $this->db->select("*")
 h3 {
   margin: 40px 0 0;
 }
-ul {
+/* ul {
   list-style-type: none;
   padding: 0;
 }
 li {
   display: inline-block;
   margin: 0 10px;
-}
+} */
 
 a {
   color: #42b983;
